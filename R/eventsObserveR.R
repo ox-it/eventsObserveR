eb0a6e1f68ddb8fbd2198a59966dd93ae126fdeb
martinjhnhadley <martinjohnhadley@gmail.com>
2016-11-22 14:33:32 +0000

    Added optional arguments and moved places into settings

--- R/eventsObserveR.R
+++ R/eventsObserveR.R
@@ -1,32 +1,56 @@
 #' @import htmlwidgets
 #' @export
 eventsObserveR <- function(events, 
-                          places, 
+                          places = NULL, 
+                          place.key = "place",
                           periodsPerSecond = 24,
                           period = 86400,
                           previousPeriodDuration = 86400,
-                          width = NULL,
-                          height = NULL) {
+                          size = list(
+                            view.width = 700,
+                            view.height = 500,
+                            interface.width = 1024, 
+                            interface.height = 786
+                          )) {
   
   # create a list that contains the settings
   settings <- list(
-    periodsPerSecond = periodsPerSecond,
+    periods_per_second = periodsPerSecond,
     period = period,
-    previousPeriodDuration = previousPeriodDuration 
+    places = places,
+    place_key = place.key,
+    view_width = size$view.width,
+    view_height = size$view.height,
+    interface_width = size$interface.width,
+    interface_height = size$interface.height,
+    previous_period_duration = previousPeriodDuration 
   )
   
   # pass the data and settings using 'x'
   x <- list(
     events = events,
-    places = places,
     settings = settings
   )
   
   # create the widget
   htmlwidgets::createWidget("eventsObserveR", 
                             x,
-                            width = width,
-                            height = height)
+                            width = size$interface.width,
+                            height = size$interface.height)
+  
+}
+
+#' @export
+eventsLegend <- function(eventsObserver = eO,
+                         legend.data = NULL,
+                         columns = 1){
+  # Add the legend to the eventsObserver
+  
+  if(is.null(legend.data)){
+    "kens_auto_legend"
+  } else {
+    "specified_appearance_legend"
+  }
 }
 
 #' @export
