9c9dff3857a2a152a4595eff7000ef9b8ef84c73
martinjhnhadley <martinjohnhadley@gmail.com>
2016-12-18 00:06:26 +0000

    Added sample events and locations

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Sample events data
+#'
+#' A sample set of observed events, one might imagine these are sightings of different species of animals at specific locations (motion-activated cameras) - identified by the `station` column. Note that this is a randomised data set and is not indicative of the true migratory behaviour of e.g. meerkats, cougars and polar bears.
+#'
+#' @format A data frame with 6621 rows and 6 variables:
+#' \describe{
+#'   \item{station}{station, unique identifier for each observation used which must be set as place.key.}
+#'   \item{time}{time, when the event was observed, in as.POSIXct format.}
+#'   \item{species}{species, species identification, i.e. cougar, kitten, stick insect.}
+#'   \item{radius}{radius, size of event when displayed.}
+#'   \item{color}{color, color for the species type}
+#'   \item{place_id}{placed_id, id of the location (place) where the event was observed, corresponding to the id in ``}
+#' }
+"sample_events_data"
+
+#' Sample locations data
+#'
+#' The locations of motion-activated camera traps that recorded the events from `sample_events_data`, with (x,y) coordinates. Note that for data sensitivity reasons the coordinates are not required to be valid longitude/latitude pairs.
+#'
+#' @format A data frame with 99 rows and 6 variables:
+#' \describe{
+#'   \item{id}{id, Unique id for each location, ranging from 0 - 98. Note that these ids correspond to the `place_id` in `sample_events_data`}
+#'   \item{x}{x, x coordinate of each locations}
+#'   \item{y}{y, y coordinate of each locations}
+#'   \item{color}{color, color of each location}
+#'   \item{radius}{radius, radiues of each location}
+#'   \item{title}{title, tooltip text displayed when hovering over the location}
+#' }
+"sample_locations_data"
---
+++ data-raw/data_emre.R
@@ -0,0 +1,37 @@
+sample_events_data <- emre_data %>%
+  select(time, Species, radius, color, place_id, title, station)
+colnames(sample_events_data) <- tolower(colnames(sample_events_data))
+
+# load("data/sample_events_data.rdata")
+# load("data/sample_locations_data.rdata")
+
+random_animal_names <- read_csv("data-raw/random-animal-names.csv")
+
+unique_species <- sample_events_data %>%
+  select(species) %>%
+  unlist(use.names = FALSE) %>%
+  trimws() %>%
+  unique()
+
+sample_events_data <- sample_events_data %>%
+         mutate(species = mapvalues(species, from = unique_species,
+                             to = sample(random_animal_names$name, length(unique_species))))
+
+new_point_coords <- rjitter(ppp(scales::rescale(sample_locations_data$x, to = c(0, 1)), scales::rescale(sample_locations_data$y, to = c(0, 1))), radius = 0.432) %>%
+  as.data.frame() %>%
+  mutate(x = round(100*x),
+         y = round(100*y))
+
+sample_locations_data$x <- new_point_coords$x
+sample_emre_locations$y <- new_point_coords$y
+
+# sample_locations_data <- sample_emre_locations
+
+sample_events_data <- sample_events_data %>%
+  select(-title)
+
+# sample_events_data <- sample_events_data %>%
+#   select(-radius)
+
+save(sample_events_data, file = "data/sample_events_data.rdata")
+save(sample_locations_data, file = "data/sample_locations_data.rdata")
