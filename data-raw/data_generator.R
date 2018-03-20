0f52e86d3ba34b553876170fcf35158803cfaeb0
martinjhnhadley <martinjohnhadley@gmail.com>
2018-03-20 14:15:51 +0000

    tidied up sample data

--- data-raw/data_generator.R
+++ data-raw/data_generator.R
@@ -2,8 +2,22 @@
 ## In the interested of replicability, the process of jittering the data points to random locations has been preserved.
 ## Additionally a set of randomly selected animal names is included in data-raw but is no longer an active part of this codebase.
 
-load("data/sample_events_data")
-load("data/sample_locations_data")
+load("data-raw/sample_events_data.rdata")
+
+sample_events_data %>% head()
+
+sample_events_data <- sample_events_data %>%
+  rename(
+    time = time,
+    event_type_id = event_type_id,
+    event_type_description = event_type,
+    place = station
+  ) %>%
+  as_data_frame()
+
+save(sample_events_data, file = "data/sample_events_data.rdata")
+
+
 library(spatstat)
 new_point_coords <- rjitter(ppp(scales::rescale(sample_locations_data$x, to = c(0, 1)), scales::rescale(sample_locations_data$y, to = c(0, 1))), radius = 0.432) %>%
   as.data.frame() %>%
@@ -12,6 +26,9 @@ new_point_coords <- rjitter(ppp(scales::rescale(sample_locations_data$x, to = c(
 
 sample_locations_data$x <- new_point_coords$x
 sample_locations_data$y <- new_point_coords$y
+
+
+sample_events_data
 
 save(sample_events_data, file = "data/sample_events_data.rdata")
 save(sample_locations_data, file = "data/sample_locations_data.rdata")
